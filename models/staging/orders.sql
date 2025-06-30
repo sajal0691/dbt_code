34dc4e0ce47e584496e8961ae377fd2f9370ce24
sajal saxena <sajal06.saxena@gmail.com>
2025-06-30 12:22:11 +0000

    commit

--- models/staging/orders.sql
+++ models/staging/orders.sql
@@ -4,6 +4,6 @@
     incremental_strategy="merge") }}
     --select * from {{source('db_table','orders_sflk')}}
     with cte1 as 
-    (select order_id , order_no , product_name , current_date as LOAD_DT
+    (select order_id , order_no , product_name , current_timestamp as LOAD_DT
     from DBT_LEARNING.DBT_LEARNING_SCHEMA.ORDERS_SFLK)
     select * from cte1
