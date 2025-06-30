d951cbf54cc46d200b44017bdcf05549fbf49495
sajal saxena <sajal06.saxena@gmail.com>
2025-06-30 12:07:28 +0000

    commit

--- models/staging/orders.sql
+++ models/staging/orders.sql
@@ -2,4 +2,8 @@
     materialized="incremental" , 
     unique_key = 'order_id' , 
     incremental_strategy="merge") }}
-    select * from {{source('db_table','orders_sflk')}}
+    --select * from {{source('db_table','orders_sflk')}}
+    with cte1 as 
+    (select order_id , order_no , product_name , current_timestamp() 
+    from DBT_LEARNING.DBT_LEARNING_SCHEMA.ORDERS_SFLK)
+    select * from cte1
