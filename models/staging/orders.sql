da6f09ecae5c9247ae867eecb1a03d2e09999d66
sajal saxena <sajal06.saxena@gmail.com>
2025-06-30 11:46:58 +0000

    commit

--- models/staging/orders.sql
+++ models/staging/orders.sql
@@ -1,5 +1,5 @@
 {{ config(
     materialized="incremental" , 
     unique_key = 'order_id' , 
-    incremental_strategy="delete+insert") }}
+    incremental_strategy="merge") }}
     select * from {{source('db_table','orders_sflk')}}
